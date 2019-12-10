5964e02dd9a0f1e087d9ccc2e44cf9312089383b
aminamos <26092352+aminamos@users.noreply.github.com>
2019-12-10 14:57:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(arg) {
+  const num = arg.split(":");
+  let num2 = num[0].concat("", num[1]);
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+  if (num2 < 1200) {
+    return "Good Morning";
+  } else if (num2 > 1200 && num2 < 1700) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(arg) {
+  let theThing = document.getElementById("greeting");
+  theThing.innerText = arg;
+}
