98c990aef9463d8959c2d3fc5ec6d8dfbe343adc
Jendnice <Pipeline38@hotmail.com>
2019-12-18 17:05:00 -0500

    Done.

---
+++ notes.js
@@ -0,0 +1,35 @@
+
+ function greet(time) {
+    if (time === (00:00 - 11:59)) {
+      return `Good morning`;
+    }
+    if (time === (12:00 - 17:00)) {
+      return `Good afternoon`;
+    }
+    if (time === (17:00 - 24:00)) {
+      return `Good afternoon`;
+    }
+  }
+   
+  let result = greet(time);
+  console.log(result);
+
+
+  /* End Given Code, don't edit above here...*/
+
+/* Write your implementation of greet() */
+function greet(time) {
+  if ((parseInt(time, 24)) < (26) {
+    return `Good morning`;
+  }
+  if ((12) < time < (5)) {
+    return `Good afternoon`;
+  }
+  if (time > (5)) {
+    return `Good afternoon`;
+  }
+}
+ 
+let result = greet(input);
+
+parseInt(time, 24)
