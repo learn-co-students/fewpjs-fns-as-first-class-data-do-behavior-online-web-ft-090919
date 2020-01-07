e4dbe7bfa4785f12a437afc2f818e1fc90daf8d2
Matthew Ream <matthewtimothyream@gmail.com>
2020-01-07 13:18:03 -0600

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let time = string.split(":");
+  let clock = parseInt(time[0]);
+  if (clock < 12) {
+    return "Good Morning";
+  } else if (clock >= 12 && clock < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string
+}
+
